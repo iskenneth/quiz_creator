488cf993049eed25591a896f5e523001bed4bc59
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-09 13:01:39 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,5 @@
+#Ask the user to input the question and 4 choices for answer
+#Ask the user to confirm the correct answer
+#Save the question+correct asnwer to .txt file 
+#Ask user if they want to input another question
+#If yes loop the program, if no break

dafa241236959e04109896b073b57085af531317
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-09 13:12:04 +0800

    inputs for questions and choices + correct answer

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,10 @@
 #Ask the user to input the question and 4 choices for answer
+question = input("Enter your question: ")
+choice_a = input("Enter answer for choice a: ")
+choice_b = input("Enter answer for choice b: ")
+choice_c = input("Enter answer for choice c: ")
+choice_d = input("Enter answer for choice d: ")
+correct answer = input("What is the letter of the correct answer?: ")
 #Ask the user to confirm the correct answer
 #Save the question+correct asnwer to .txt file 
 #Ask user if they want to input another question

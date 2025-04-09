19ab8a37961cad9162b70fabbe7af66be969aa54
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-09 22:14:56 +0800

    importing buttons and text input of GUI

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,9 @@
 from kivy.app import app
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button 
+from kivy.uix.spinner import Spinner
 
 while True:
     #Ask the user to input the question and 4 choices for answer

b63ccdd791fc6c9c548fb652f05064bef001948f
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-09 14:17:33 +0800

    converting inputs to text files

--- quiz_creator.py
+++ quiz_creator.py
@@ -6,5 +6,7 @@ choice_c = input("Enter answer for choice c: ")
 choice_d = input("Enter answer for choice d: ")
 correct_answer = input("What is the letter of the correct answer?: ") #Ask the user to confirm the correct answer
 #Save the question+correct asnwer to .txt file 
+with open('quiz_data.txt' , 'a') as file:
+    file.write(f" Question: {question} \n")
 #Ask user if they want to input another question
 #If yes loop the program, if no break

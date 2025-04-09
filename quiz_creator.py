b3d9696f525361acde49d79a2ad74fa0768a49fd
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-09 13:16:58 +0800

    replace the space in correct answer with _

--- quiz_creator.py
+++ quiz_creator.py
@@ -4,8 +4,7 @@ choice_a = input("Enter answer for choice a: ")
 choice_b = input("Enter answer for choice b: ")
 choice_c = input("Enter answer for choice c: ")
 choice_d = input("Enter answer for choice d: ")
-correct answer = input("What is the letter of the correct answer?: ")
-#Ask the user to confirm the correct answer
+correct_answer = input("What is the letter of the correct answer?: ") #Ask the user to confirm the correct answer
 #Save the question+correct asnwer to .txt file 
 #Ask user if they want to input another question
 #If yes loop the program, if no break

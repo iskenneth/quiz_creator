f5521a434a911f5d299839a0c72de914baccb741
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-05-07 14:36:01 +0800

    checking if the answer is right

--- answer_quiz.py
+++ answer_quiz.py
@@ -5,6 +5,8 @@ class Question:
         self.choices = choices
         self.correct_answer = correct_answer
         
+    def is_correct (self, answer):
+        return self.correct_answer == answer
         
 #When creating: a. Ask for the question text  b. Ask for 4 choices (A, B, C, D) c. Ask which letter is the correct answer
 # Store the question text, choices, and correct answer

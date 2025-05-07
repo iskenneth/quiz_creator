0b8090dd6a5689c4fa213120e133dda1b6e7178a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-05-07 14:11:05 +0800

    pseudocode

---
+++ answer_quiz.py
@@ -0,0 +1,9 @@
+#Create a new question 
+#When creating: a. Ask for the question text  b. Ask for 4 choices (A, B, C, D) c. Ask which letter is the correct answer
+# Store the question text, choices, and correct answer
+#Show the question and choices to the user
+#Ask the user to enter their answer
+#Compare the user's answer to the correct answer
+# If the answer is correct, say "Correct!" Else, say "Wrong!"
+
+        

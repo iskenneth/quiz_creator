d62732c118363ddd498042fd793392a5e24ce0bc
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-05-07 20:22:18 +0800

    opening file with read function

--- answer_quiz.py
+++ answer_quiz.py
@@ -9,11 +9,7 @@ class Question:
         return self.correct_answer == answer
         
     def load_question_from_file(quiz_data.txt):
+        question = []
+    with open(quiz_data.txt, 'r') as file:
+        lines = file.readlines()
         
-        
-#When creating: a. Ask for the question text  b. Ask for 4 choices (A, B, C, D) c. Ask which letter is the correct answer
-# Store the question text, choices, and correct answer
-#Show the question and choices to the user
-#Ask the user to enter their answer
-#Compare the user's answer to the correct answer
-# If the answer is correct, say "Correct!" Else, say "Wrong"  
